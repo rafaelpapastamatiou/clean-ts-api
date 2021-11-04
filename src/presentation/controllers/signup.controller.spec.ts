f7cb0ba7ed9d71eaf20ff312ea5a34ae7f3d072d
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-03 22:46:36 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from "./signup.controller";
+
+describe("SignUp Controller", () => {
+  test("should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        email: "johndoe@email.com",
+        password: "123456",
+        passwordConfirmation: "123456",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
