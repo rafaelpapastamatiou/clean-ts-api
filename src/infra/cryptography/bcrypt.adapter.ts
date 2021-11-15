fb32634d3dd35589522d118b2b190aea3f302ba0
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-15 19:36:02 -0300

    feat: ensure BcryptAdapter returns value hash on success

--- src/infra/cryptography/bcrypt.adapter.ts
+++ src/infra/cryptography/bcrypt.adapter.ts
@@ -6,8 +6,8 @@ export class BcryptAdapter implements IEncrypter {
   constructor(private salt: number) {}
 
   async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
+    const hashedValue = await bcrypt.hash(value, this.salt);
 
-    return "";
+    return hashedValue;
   }
 }
