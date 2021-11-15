6aa45e3fab733354fc36f0dec6ae2cd6f47516ee
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-15 20:31:28 -0300

    feat: ensure MongoAccountRepository returns an account on success

---
+++ src/infra/database/mongodb/repositories/account/account.repository.ts
@@ -0,0 +1,23 @@
+import { IAddAccountRepository } from "../../../../../data/protocols/add-account.repository";
+import { IAccountModel } from "../../../../../domain/models/account";
+import { IAddAccountModel } from "../../../../../domain/useCases/add-account";
+import { MongoHelper } from "../../helpers/mongo-helper";
+
+export class MongoAccountRepository implements IAddAccountRepository {
+  async add(data: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+
+    const result = await accountCollection.insertOne(data, {});
+
+    const account = await accountCollection.findOne(result.insertedId);
+
+    if (!account) throw new Error("Error saving account to mongodb.");
+
+    const { _id, ...accountData } = account;
+
+    return {
+      id: _id,
+      ...accountData,
+    } as IAccountModel;
+  }
+}
