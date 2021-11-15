a80e6663f93a278eb02c35339998a90a210331be
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-15 20:30:58 -0300

    chore: update jest-mongodb configs

--- jest.config.ts
+++ jest.config.ts
@@ -192,7 +192,7 @@ export default {
   // verbose: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
+  watchPathIgnorePatterns: ["globalConfig"],
 
   // Whether to use watchman for file crawling
   // watchman: true,
