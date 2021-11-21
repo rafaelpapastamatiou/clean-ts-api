40ea742349728ed89150132a7e3db94b65a0631a
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-21 16:45:58 -0300

    test: ensure MongoAccountRepository throws if mongodb can't find created account after insert

--- src/infra/database/mongodb/repositories/account/account.repository.spec.ts
+++ src/infra/database/mongodb/repositories/account/account.repository.spec.ts
@@ -1,3 +1,5 @@
+import { Collection, Document } from "mongodb";
+
 import { MongoHelper } from "../../helpers/mongo-helper";
 import { MongoAccountRepository } from "./account.repository";
 
@@ -33,5 +35,29 @@ describe("MongoDb Account Repository", () => {
     expect(account.name).toBe("any_name");
     expect(account.email).toBe("any_email@email.com");
     expect(account.password).toBe("any_password");
+  });
+
+  test("should throw if mongodb can't find created account after insert", async () => {
+    const sut = makeSut();
+
+    jest
+      .spyOn(MongoHelper, "getCollection")
+      .mockImplementationOnce((name: string): Collection<Document> => {
+        const collection = MongoHelper.getCollection(name);
+
+        collection.findOne = async () => {
+          return null;
+        };
+
+        return collection;
+      });
+
+    const promise = sut.add({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+    });
+
+    await expect(promise).rejects.toThrow();
   });
 });
