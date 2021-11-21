b0a8634aa00ccd56216fe1fb4799f11c88304fcb
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-21 17:09:28 -0300

    refactor: add MongoHelper interface

--- src/infra/database/mongodb/helpers/mongo-helper.ts
+++ src/infra/database/mongodb/helpers/mongo-helper.ts
@@ -4,17 +4,29 @@ type MongoMappedDocument = {
   id: string;
 };
 
-export const MongoHelper = {
+interface IMongoHelper {
+  client: MongoClient;
+  connect: (uri: string) => Promise<void>;
+  disconnect: () => Promise<void>;
+  getCollection: (name: string) => Collection;
+  map: <T extends MongoMappedDocument>(doc: Document) => T;
+}
+
+export const MongoHelper: IMongoHelper = {
   client: null as unknown as MongoClient,
+
   async connect(uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri);
   },
+
   async disconnect(): Promise<void> {
     await this.client.close();
   },
+
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
   },
+
   map: <T extends MongoMappedDocument>(document: Document): T => {
     const { _id, ...rest } = document;
 
