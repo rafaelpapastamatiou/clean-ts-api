803c1a1d99347c2e547fb8733ff49d8d9eac974e
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-21 16:02:13 -0300

    feat: connect to mongodb on startup

---
+++ src/main/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/clean-note-api",
+  port: process.env.PORT || "4000",
+};
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,16 @@
-import { app } from "./app";
+import { MongoHelper } from "../infra/database/mongodb/helpers/mongo-helper";
+import { env } from "./env";
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+(async () => {
+  try {
+    await MongoHelper.connect(env.mongoUrl);
+
+    const { app } = await import("./app");
+
+    app.listen(env.port, () =>
+      console.log(`Server running at http://localhost:${env.port.toString()}`)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
