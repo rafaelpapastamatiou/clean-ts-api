3a4d3836fa3d1d7d28df7c14511782e19445f141
Rafael Papastamatiou <rafaelpapas1412@gmail.com>
2021-11-23 22:53:58 -0300

    refactor: move sut creation to a factory method

--- src/presentation/validators/validation.composite.spec.ts
+++ src/presentation/validators/validation.composite.spec.ts
@@ -1,18 +1,39 @@
 import { IValidation } from "./validation";
 import { ValidationComposite } from "./validation.composite";
 
+interface IMakeSut {
+  sut: ValidationComposite;
+  validationStub: IValidation;
+}
+
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    async validate(_data: Record<string, unknown>): Promise<Error | undefined> {
+      return undefined;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+const makeSut = (): IMakeSut => {
+  const validationStub = makeValidation();
+
+  const sut = new ValidationComposite([validationStub]);
+
+  return {
+    sut,
+    validationStub,
+  };
+};
+
 describe("ValidationComposite", () => {
   test("should return an error if any validation fails", async () => {
-    class ValidationStub implements IValidation {
-      async validate(
-        _data: Record<string, unknown>
-      ): Promise<Error | undefined> {
-        return new Error();
-      }
-    }
-    const validationStub = new ValidationStub();
+    const { validationStub, sut } = makeSut();
 
-    const sut = new ValidationComposite([validationStub]);
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new Promise((resolve) => resolve(new Error())));
 
     const error = await sut.validate({
       field: "any_value",
